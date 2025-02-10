4511c88390a83137be69ddccfa8526df377216e3
simonbaeke <simonbaeke@hotmail.com>
2025-02-10 23:01:30 +0100

    create superuser

--- blijmare/home/migrations/0001_initial.py
+++ blijmare/home/migrations/0001_initial.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("wagtailcore", "0040_page_draft_title"),
+
     ]
 
     operations = [
