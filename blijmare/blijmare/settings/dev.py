7a965e2bf61a5d82700b9ac9a5afe3fc8da33e24
simonbaeke <simonbaeke@hotmail.com>
2025-02-10 21:05:09 +0100

    first commit

---
+++ blijmare/blijmare/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-u(+#a0f5rnv4=np@uwnl0tkn22!ahkq7x@i63z7^eg6cs=q28("
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
