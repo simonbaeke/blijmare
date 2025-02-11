9f9066a7a2750a5f3881cc7f751d2412b5620867
simonbaeke <simonbaeke@hotmail.com>
2025-02-12 00:44:02 +0100

    manage.py

--- blijmare/blijmare/settings/production.py
+++ blijmare/blijmare/settings/production.py
@@ -13,7 +13,7 @@ except ImportError:
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://root:eJsFuQBFcKFzLbs8ZxPHHKIyLjRbq828@dpg-culr33jqf0us73di0m00-a/blijmaredb',
         conn_max_age=600
     )
 }
