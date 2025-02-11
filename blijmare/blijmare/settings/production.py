a344f294923fbcfd39bc2e9c3199a0206af436d6
simonbaeke <simonbaeke@hotmail.com>
2025-02-11 22:21:45 +0100

    new deploy

--- blijmare/blijmare/settings/production.py
+++ blijmare/blijmare/settings/production.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 from .base import *
 
 DEBUG = False
@@ -6,3 +8,12 @@ try:
     from .local import *
 except ImportError:
     pass
+
+# Replace the SQLite DATABASES configuration with PostgreSQL:
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
+}
